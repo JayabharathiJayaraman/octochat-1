67191185d7ac52c980d38a39c1353e0290f1cdeb
JayabharathiJayaraman <jayabharathi.jayaraman@iths.se>
2020-11-11 16:59:29 +0100

    Created user profile

---
+++ UserProfile/app/src/main/java/com/example/userprofile/SettingsActivity.kt
@@ -0,0 +1,25 @@
+package com.example.userprofile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class SettingsActivity : AppCompatActivity() {
+
+     var linerAcc : LinearLayout ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+    linerAcc = findViewById(R.id.layoutAccount)
+        linerAcc?.setOnClickListener{
+            val intent = Intent(this, Account::class.java)
+            startActivity(intent)
+
+
+
+        }
+    }
+}
